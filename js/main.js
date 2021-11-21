d8eb4cde1e829b0ab7b1d02c5ea9a1843aed215f
mehediemon007 <mehediemon7@gmail.com>
2021-11-21 16:36:45 +0600

    thikana homepage

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 ;(function($){
 
+    $(".nav-toggle").click(function(){
+        $(".thk-main-menu").slideToggle("100");
+    })
+
     $(".single-slider").owlCarousel({
         items: 1,
         dots:false,
-        nav:true,
+        nav:false,
         navText:["<i class='fas fa-angle-left'></i>","<i class='fas fa-angle-right'></i>"],
         autoplay: true,
         smartSpeed: 700,
         loop: true,
         autoplayHoverPause: true,
+        responsive:{
+           1200:{
+               nav:true
+           }
+        }
+    });
+
+     // Magnifig Popup
+     $('.videolink').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
     });
 
 })(jQuery);
